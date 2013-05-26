d28dd7ad9be38ef976ed4c57f8a7af8c4a42989a
Vince Lane <vince@vincelane.com>
2013-05-25 18:26:31 -0700

    Trying out different header style

--- js/disqus-web/style-guide/style-guide-core.js
+++ js/disqus-web/style-guide/style-guide-core.js
@@ -1,3 +1,16 @@
-$(function() {
-    prettyPrint();
+function headerNav() {
+	$(window).scroll(function () {
+		var scrollPosition = $(document).scrollTop();
+		var header = $(".global-nav");
+		if ( scrollPosition > 0 ) {
+			header.addClass("short");
+		} else {
+			header.removeClass("short");
+		}
+	})
+}
+
+$(document).ready(function() {
+	prettyPrint();
+	headerNav();
 });
